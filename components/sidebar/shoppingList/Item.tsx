0b91121c116f9b866c31430285f6bf7cab89785a
dvqc-office <bra-95@live.fr>
2022-11-27 15:10:44 +0100

    added button group to modify item qty

--- components/sidebar/shoppingList/Item.tsx
+++ components/sidebar/shoppingList/Item.tsx
@@ -1,22 +1,35 @@
 import { useState } from "react";
 import { ListItemData } from "types/prisma.types";
 
-const Item = ({ key, listItem }: { key: string; listItem: ListItemData }) => {
+const Item = ({ listItem }: { listItem: ListItemData }) => {
   const [itemChecked, setItemChecked] = useState(false);
   const checkItem = () => {
     setItemChecked(!itemChecked);
   };
   return (
-    <li key={key} className="my-4 max-h-min flex flex-row items-center">
-      <label className="flex mr-4">
+    <li key={listItem.id} className="my-4 max-h-min flex flex-row items-center">
+      <label className="flex mr-4 cursor-pointer">
         <input className="hidden" type="checkbox" onChange={checkItem} checked={itemChecked} />
         <span className="h-6 w-6 inline-block relative border-2 border-yellow1 rounded-[4px]"></span>
       </label>
-      <p className={`text-lg font-medium text-black ${itemChecked ? "line-through" : ""}`}>{listItem.item.name}</p>
-      <button className="h-8 w-16 mr-0 ml-auto bg-none text-xs font-bold text-yellow1  border-yellow1 border-2 rounded-3xl ">
-        {listItem.qty}
-        {" pcs"}
-      </button>
+      <p className={`text-lg font-medium text-black  break-words ${itemChecked ? "line-through" : ""}`}>
+        {listItem.item.name}
+      </p>
+      <div className="h-10 flex flex-row mr-0 ml-auto bg-none text-xs font-bold bg-white rounded-xl">
+        <button
+          className="w-8 bg-[url('/images/delete.svg')] bg-yellow1 bg-no-repeat bg-[length:60%_60%] 
+        bg-center rounded-xl"
+        ></button>
+        <button className="w-8 bg-[url('/images/minus.svg')]  bg-no-repeat bg-[length:60%_60%] bg-center"></button>
+        <button
+          className="w-14 text-yellow1  
+        border-yellow1 border-2 rounded-3xl "
+        >
+          {listItem.qty}
+          {" pcs"}
+        </button>
+        <button className="w-8 bg-[url('/images/addPcs.svg')]  bg-no-repeat bg-[length:60%_60%] bg-center"></button>
+      </div>
     </li>
   );
 };
--- components/sidebar/shoppingList/ShoppingList.tsx
+++ components/sidebar/shoppingList/ShoppingList.tsx
@@ -14,22 +14,29 @@ const ShoppingList = () => {
 
   return (
     <div className="w-full h-full m-0 p-0 flex flex-col">
-      <div className="w-full m-0 px-10 pt-8 grow">
-        <AddItem></AddItem>
-        <div className="m-0 p-0 w-full">
-          {!list ? (
-            <Loader></Loader>
-          ) : (
-            <>
+      {!list ? (
+        <Loader></Loader>
+      ) : (
+        <>
+          <div className="w-full m-0 px-10 pt-8 grow">
+            <AddItem></AddItem>
+            <div className="m-0 p-0 w-full">
               <h2 className="my-10 text-2xl font-bold text-dark2">{list.name}</h2>
               <ItemsList list={list} isEditing={isEditing}></ItemsList>
-            </>
-          )}
-        </div>
-      </div>
-      <div className="w-full px-10 py-8 bg-white ">
-        <NameInput disabled={!isEditing}></NameInput>
-      </div>
+            </div>
+          </div>
+          <div className="w-full px-10 py-8 bg-white ">
+            {isEditing ? (
+              <NameInput disabled={list.listItems.length == 0}></NameInput>
+            ) : (
+              <div className="btn-group">
+                <button className="btn bg-gray5 text-dark2">Cancel</button>
+                <button className="btn bg-blue1 text-white">Complete</button>
+              </div>
+            )}
+          </div>
+        </>
+      )}
     </div>
   );
 };
