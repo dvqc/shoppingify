5bc27a3c6d8e74ee1fd40fe4ff1ca85e93677a59
Brahim <bra-95@live.fr>
2022-12-30 20:21:58 +0100

    fixed skeleton loader styling

--- components/loader/SkeletonLoader.tsx
+++ components/loader/SkeletonLoader.tsx
@@ -1,4 +1,4 @@
 const SkeletonLoader = ({ className }: { className?: string }) => {
-  return <div className={`w-full h-full rounded-2xl bg-slate-100 opacity-70 animate-pulse ${className}`}></div>;
+  return <div className={`${className ?? ""} skeleton`}></div>;
 };
 export default SkeletonLoader;
--- pages/history/index.tsx
+++ pages/history/index.tsx
@@ -1,10 +1,7 @@
-import Empty from "components/Empty";
 import { HistoryContainer } from "components/history";
-import MonthLists from "components/history/MonthLists";
 import { SkeletonLoader } from "components/loader";
 import { useLists } from "hooks/queries";
 import { NextPage } from "next";
-import { ListData } from "types/prisma";
 
 const History: NextPage = () => {
   const { data: lists, error } = useLists();
@@ -15,45 +12,45 @@ const History: NextPage = () => {
         <div>failed to load</div>;
       </HistoryContainer>
     );
-  if (!lists)
-    return (
-      <HistoryContainer>
-        <SkeletonLoader className="h-8 w-32 mt-10 bg-slate-200" />
-        <div className="flex flex-col gap-5 my-6">
-          <SkeletonLoader className="h-14  bg-slate-200" />
-          <SkeletonLoader className="h-14  bg-slate-200" />
-          <SkeletonLoader className="h-14  bg-slate-200" />
-        </div>
-        <SkeletonLoader className="h-8 w-32 mt-10 bg-slate-200" />
-        <div className="flex flex-col gap-5 my-6">
-          <SkeletonLoader className="h-14  bg-slate-200" />
-          <SkeletonLoader className="h-14  bg-slate-200" />
-          <SkeletonLoader className="h-14  bg-slate-200" />
-        </div>
-      </HistoryContainer>
-    );
 
-  const filteredList = lists.filter((list) => list.status != "ACTIVE");
-  const listsPerMonth: ListData[][] = [];
-
-  if (filteredList.length > 0) {
-    listsPerMonth.push([lists[0]]);
-    for (let i = 1; i < filteredList.length; i++) {
-      const monthOfPrevious = new Date(filteredList[i - 1].createdAt).getMonth();
-      const monthOfCurrent = new Date(filteredList[i].createdAt).getMonth();
-      if (monthOfCurrent !== monthOfPrevious) listsPerMonth.push([filteredList[i]]);
-      else listsPerMonth[listsPerMonth.length - 1].push(filteredList[i]);
-    }
-  }
   return (
     <HistoryContainer>
-      {listsPerMonth.length > 0 ? (
-        listsPerMonth.map((monthLists, i) => <MonthLists key={i} lists={monthLists}></MonthLists>)
-      ) : (
-        <Empty classname="h-3/4">You have no lists in your history</Empty>
-      )}
+      <SkeletonLoader className="h-8 w-32 mt-10 bg-slate-200" />
+      <div className="flex flex-col gap-5 my-6">
+        <SkeletonLoader className="h-14 rounded-xl bg-slate-200" />
+        <SkeletonLoader className="h-14 rounded-xl bg-slate-200" />
+        <SkeletonLoader className="h-14 bg-slate-200" />
+      </div>
+      <SkeletonLoader className="h-8 w-32 mt-10 bg-slate-200" />
+      <div className="flex flex-col gap-5 my-6">
+        <SkeletonLoader className="h-14  bg-slate-200" />
+        <SkeletonLoader className="h-14  bg-slate-200" />
+        <SkeletonLoader className="h-14  bg-slate-200" />
+      </div>
     </HistoryContainer>
   );
+
+  // const filteredList = lists.filter((list) => list.status != "ACTIVE");
+  // const listsPerMonth: ListData[][] = [];
+
+  // if (filteredList.length > 0) {
+  //   listsPerMonth.push([lists[0]]);
+  //   for (let i = 1; i < filteredList.length; i++) {
+  //     const monthOfPrevious = new Date(filteredList[i - 1].createdAt).getMonth();
+  //     const monthOfCurrent = new Date(filteredList[i].createdAt).getMonth();
+  //     if (monthOfCurrent !== monthOfPrevious) listsPerMonth.push([filteredList[i]]);
+  //     else listsPerMonth[listsPerMonth.length - 1].push(filteredList[i]);
+  //   }
+  // }
+  // return (
+  //   <HistoryContainer>
+  //     {listsPerMonth.length > 0 ? (
+  //       listsPerMonth.map((monthLists, i) => <MonthLists key={i} lists={monthLists}></MonthLists>)
+  //     ) : (
+  //       <Empty classname="h-3/4">You have no lists in your history</Empty>
+  //     )}
+  //   </HistoryContainer>
+  // );
 };
 
 export default History;
